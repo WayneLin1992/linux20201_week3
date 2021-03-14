a1c335c02a7126e8db64d86b98f1fcd64b026025
WayneLin1992 <62940642+WayneLin1992@users.noreply.github.com>
2021-03-14 18:56:15 +0800

    Add the file of the quiz3
    mission: add the file of the quiz3

---
+++ fbstring.c
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "fbstring.h"
+
+int main(int argc, char *argv[])
+{
+    xs string = *xs_tmp("\n foobarbar \n\n\n");
+    xs_trim(&string, "\n ");
+    printf("[%s] : %2zu\n", xs_data(&string), xs_size(&string));
+
+    xs prefix = *xs_tmp("((("), suffix = *xs_tmp(")))");
+    xs_concat(&string, &prefix, &suffix);
+    printf("[%s] : %2zu\n", xs_data(&string), xs_size(&string));
+    return 0;
+}
